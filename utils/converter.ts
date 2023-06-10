0e7d64a558eca001931992c36838451784a15e65
Mike Rodrigues De Lima <mike.rodrigues.lima@gmail.com>
2023-06-09 22:41:54 -0300

    Update converter.ts

--- utils/converter.ts
+++ utils/converter.ts
@@ -18,21 +18,16 @@ export class PinoRequestConverter {
 
     let query = '';
     if (hasQueryParams.length) {
-      query = hasQueryParams[0];
+      query = hasQueryParams[0] as string;
       request.url = request.url.substring(0, request.url.lastIndexOf('?'));
     }
 
     const rawBody = request?.raw?.body
 
-    const body = `--data-raw '${JSON.stringify(rawBody)}'`;
-
-    const paramsUrl = `${request?.params ? params : ''}`;
-
+    const body = `--data-raw '${rawBody ? JSON.stringify(rawBody) : undefined}'`;
+    const paramsUrl = `${(request === null || request === void 0 ? void 0 : request.params) ? params : ''}`;
     const protocol = request.raw.protocol;
-
-    const curl = `curl --location -g --request ${request.method.toUpperCase()} '${protocol + '://' + request.headers.host + request.url + paramsUrl + query
-      }' ${header} ${Object.keys(rawBody).length ? body : ''}`;
-
+    const curl = `curl --location -g --request ${request.method.toUpperCase()} '${protocol + '://' + request.headers.host + request.url + paramsUrl + query}' ${header} ${Object.keys(rawBody)?.length ? body : ''}`;
     return curl.trim();
   }
 
