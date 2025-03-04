5526c3af6a843fc10ca8dfba44ee5106bdf857ab
Mike Lima <mike.rodrigues.lima@gmail.com>
2025-03-04 18:04:34 -0300

    add new version

--- src/converter.d.ts
+++ src/converter.d.ts
@@ -1,29 +1,31 @@
 import { Types } from './entity';
 
 declare module 'convert-pino-request-to-curl' {
-  /**
- * Converts an Axios error request into a cURL command.
- *
- * @param request - The Axios request object containing the request details.
- * @param anonymizedFields - An array of field names that should be anonymized in the output.
- * @returns A string representing the cURL command equivalent of the request.
- *
- * @example
- * ```typescript
- * import { getCurl } from 'convert-pino-request-to-curl';
- * 
- * const request = {
- *   method: 'POST',
- *   url: 'https://api.example.com/data',
- *   headers: { Authorization: 'Bearer token' },
- *   data: { sensitiveField: 'secretValue' },
- * };
- * 
- * const anonymizedFields = ['Authorization', 'sensitiveField'];
- * 
- * const curlCommand = getCurl(request, anonymizedFields);
- * console.log(curlCommand);
- * ```
- */
-  export function getCurl(request: Types.RequestType, anonymizedFields: string[]): string;
+  export class PinoRequestConverter {
+    /**
+   * Converts an Axios error request into a cURL command.
+   *
+   * @param request - The Axios request object containing the request details.
+   * @param anonymizedFields - An array of field names that should be anonymized in the output.
+   * @returns A string representing the cURL command equivalent of the request.
+   *
+   * @example
+   * ```typescript
+   * import { getCurl } from 'convert-pino-request-to-curl';
+   * 
+   * const request = {
+   *   method: 'POST',
+   *   url: 'https://api.example.com/data',
+   *   headers: { Authorization: 'Bearer token' },
+   *   data: { sensitiveField: 'secretValue' },
+   * };
+   * 
+   * const anonymizedFields = ['Authorization', 'sensitiveField'];
+   * 
+   * const curlCommand = getCurl(request, anonymizedFields);
+   * console.log(curlCommand);
+   * ```
+   */
+    static getCurl(request: Types.RequestType, anonymizedFields: string[]): string;
+  }
 }
